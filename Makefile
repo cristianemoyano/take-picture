c5fbef933d757b51cd47a4a47471739d09ee41b4
cmoyano <cristianmoyano.mza@gmail.com>
2019-07-20 14:42:00 -0300

    fix file path

--- Makefile
+++ Makefile
@@ -1,5 +1,17 @@
-run:
+django:
 	python manage.py runserver
+
+redis:
+	redis-server
+
+celery:
+	celery worker --app=core.celery -l info
 
 e:
 	source env/bin/activate
+
+h-celery:
+	heroku logs -t -p worker
+
+h-logs:
+	heroku logs --tail
--- snapshot/utils.py
+++ snapshot/utils.py
@@ -2,6 +2,7 @@ import os
 import json
 import requests
 
+from django.conf import settings
 import redis
 
 from celery.result import AsyncResult
@@ -12,7 +13,8 @@ import logging
 
 def upload(filename):
     api_url = 'http://patent-recognizer.herokuapp.com/'
-    with open(filename, 'rb') as file:
+    file_path = os.path.join(settings.BASE_DIR, filename)
+    with open(file_path, 'rb') as file:
         multipart_data = MultipartEncoder(
             fields={
                 '': (filename, file, 'image/png'),
